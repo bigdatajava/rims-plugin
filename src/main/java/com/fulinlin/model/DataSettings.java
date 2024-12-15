f2eb5c5574057fee566c054e28e5748da45ebb37
bigdatajava <bigdatajava@163.com>
2024-12-15 23:14:30 +0800

    'model-version'

---
+++ src/main/java/com/fulinlin/model/DataSettings.java
@@ -0,0 +1,31 @@
+package com.fulinlin.model;
+
+import java.util.List;
+
+/**
+ * @program: git-commit-message-helper
+ * @author: fulin
+ * @create: 2019-12-05 21:22
+ **/
+public class DataSettings {
+    private String template;
+    private List<TypeAlias> typeAliases;
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    public List<TypeAlias> getTypeAliases() {
+        return typeAliases;
+    }
+
+    public void setTypeAliases(List<TypeAlias> typeAliases) {
+        this.typeAliases = typeAliases;
+    }
+
+
+}
